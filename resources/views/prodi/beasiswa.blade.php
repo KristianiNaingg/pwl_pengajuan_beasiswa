e06da17697c3e5894f043bf7257b7e83804feaea
Theresia <90782555+2172017@users.noreply.github.com>
2024-06-07 16:49:51 +0700

    Update and rename hasilpolling.blade.php to beasiswa.blade.php

--- resources/views/prodi/beasiswa.blade.php
+++ resources/views/prodi/beasiswa.blade.php
@@ -7,12 +7,13 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Hasil Polling</h1>
+                        <h1 class="m-0">Pengajuan Beasiswa</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Hasil polling detail</li>
+                            <li class="breadcrumb-item active">Program Studi</li>
+
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -21,35 +22,36 @@
         <!-- /.content-header -->
 
         <!-- Main content -->
+
+        <div class="card-footer">
+            <div class="container-fluid">
+            </div>
+
+
+        </div>
         <div class="content">
             <div class="container-fluid">
                 <div class="card p-4">
                     <div class="card-body">
                         <table id="table-matkul" class="table table-striped">
                             <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Status Pemilihan</th>
-                                    <th>Nama Mahasiswa Pemilih</th>
-                                    <th>id Mata Kuliah</th>
-                                    <th>id Polling</th> <!-- Fixed the missing closing tag -->
-                                </tr>
+                            <tr>
+                                <th>Id Pengajuan </th>
+                                <th>Id Jenis Beasiswa</th>
+                                <th>Id Periode</th>
+                                <th>Id Prodi</th>
+                                <th>Ipk Terakhir</th>
+                                <th>Semester Pengajuan</th>
+                                <th>Status Pengajuan</th>
+                                <th>Alasan Ditolak</th>
+
+
+
+
+
+                            </tr>
                             </thead>
-                            <tbody>
-                                @foreach($hasilpollings as $hasilpolling)
-                                    <tr>
-                                        <td>{{ $hasilpolling->id_detail }}</td>
-                                        <td>{{ $hasilpolling->status_pemilihan }}</td>
-                                        <td>{{ $hasilpolling->mahasiswa->name }}</td>
-                                        <td>{{ $hasilpolling->id_matkul}}</td>
-                                        <td>{{ $hasilpolling->id_polling}}</td>
 
-                                        
-
-                                        
-                                    </tr>
-                                @endforeach
-                            </tbody>
                         </table>
                     </div>
                 </div>
@@ -61,7 +63,7 @@
 
 @section('ExtraCSS')
     <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('project-03/public/plugins/sweetalert2/sweetalert2.css') }}">
+    <link rel="stylesheet" href="{{asset('project-03/public/plugins/sweetalert2/sweetalert2.css')}}">
 @endsection
 
 @section('ExtraJS')
