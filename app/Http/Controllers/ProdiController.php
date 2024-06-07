a02bb3114c6de9a3f59dde3e970e3521f6996eb6
Theresia <90782555+2172017@users.noreply.github.com>
2024-06-07 16:44:16 +0700

    ProdiController.php

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -1,74 +1,41 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Matakuliah;
+
 
 use App\Models\Prodi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 class ProdiController extends Controller
 {
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('programstudi.index', [
-            'prodis' => Prodi::all(),
-        ]);
-    }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $matkuls = Matakuliah::with('kurikulum')->get();
+        return view('prodi.index', compact('matkuls'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    public function create()
+
+    {
+
+    }
+
     public function store(Request $request)
     {
-        //
+
+
+
+
     }
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Prodi $prodi
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Prodi $prodi)
+    public function showbeasiswa()
     {
-        //
+        return view('prodi.beasiswa');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Prodi $prodi
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Prodi $prodi)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Prodi $prodi
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Prodi $prodi)
-    {
-        //
-    }
+
+
+
 }
