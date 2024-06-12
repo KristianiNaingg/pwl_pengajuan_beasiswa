7d39f78183216be137a84d09ccb6a2eae42e8a60
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2024-06-12 09:44:29 +0700

    Menambahkan model baru JenisBeasiswa

---
+++ app/Models/JenisBeasiswa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisBeasiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jenis_beasiswa'; #value yang dimasukan adalah nama tabelnya
+
+    protected $fillable = [
+        'id_jenis_beasiswa',
+        'nama_jenis_beasiswa',
+        'deskripsi'
+    ];
+
+    protected $primaryKey = 'id_jenis_beasiswa';
+}
