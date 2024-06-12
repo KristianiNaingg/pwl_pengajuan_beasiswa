5e1ae348183b6d3b6a9e936e5b8d0d1c5cb59d5a
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2024-06-12 09:43:15 +0700

    Menambahkan JenisBeasiswaController

---
+++ app/Http/Controllers/JenisBeasiswaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JenisBeasiswa;
+use Illuminate\Http\Request;
+
+class JenisBeasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JenisBeasiswa  $jenisBeasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JenisBeasiswa $jenisBeasiswa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JenisBeasiswa  $jenisBeasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JenisBeasiswa $jenisBeasiswa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JenisBeasiswa  $jenisBeasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JenisBeasiswa $jenisBeasiswa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JenisBeasiswa  $jenisBeasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JenisBeasiswa $jenisBeasiswa)
+    {
+        //
+    }
+}
