fa35e8e1ce4a57e5888536a6fb17ea69685715cc
Theresia <90782555+2172017@users.noreply.github.com>
2024-06-23 19:01:45 +0700

    Update ProdiController.php

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -1,41 +1,54 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Matakuliah;
 
-
+use App\Http\Middleware\Mahasiswa;
+use App\Models\PengajuanBeasiswa;
 use App\Models\Prodi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class ProdiController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
     public function index()
     {
-        $matkuls = Matakuliah::with('kurikulum')->get();
-        return view('prodi.index', compact('matkuls'));
+        $prodi = Prodi::all();
+        return view('user_prodi.index', compact('prodi'));
     }
 
-    public function create()
-
+    public function pengajuan()
     {
+        $pengajuan = PengajuanBeasiswa::all();
+        return view('user_prodi.pengajuanbeasiswa', compact('pengajuan'));
+    }
 
+
+
+    public function create()
+    {
+        return view('prodi.create');
     }
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'nama_prodi' => 'required|string|max:255',
+            'id_fakultas' => 'required|integer',
+            'id_dosen' => 'required|integer',
+        ]);
 
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
 
+        Prodi::create($request->all());
 
-
+        return redirect()->route('prodi.index')->with('success', 'Prodi berhasil ditambahkan');
     }
-    public function showbeasiswa()
-    {
-        return view('prodi.beasiswa');
-    }
-
-
-
-
-
 }
