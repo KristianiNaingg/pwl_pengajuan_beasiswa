ce5e14f6ba204f847df2f5381b121e2f04f3cdc7
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2024-06-24 22:49:05 +0700

    Update PengajuanBeasiswa.php

--- app/Models/PengajuanBeasiswa.php
+++ app/Models/PengajuanBeasiswa.php
@@ -12,18 +12,14 @@ class PengajuanBeasiswa extends Model
     protected $table = 'pengajuan_beasiswa'; #value yang dimasukan adalah nama tabelnya
 
     protected $fillable = [
-        'id_pengajuan',
+        'NRP',
         'status_pengajuan',
         'semester_pengajuan',
-        'alasan_ditolak',
         'transkrip_nilai',
         'surat_rekomendasi',
         'surat_pernyataan',
         'surat_keaktifan',
         'surat_keterangan',
-        'id_periode',
-        'id_beasiswa',
-        'NRP',
     ];
 
     protected $primaryKey = 'id_pengajuan';
