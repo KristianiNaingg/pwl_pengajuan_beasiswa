9f6b23a107ecdda48fe2c20e513bcbe9b7ade832
Theresia <90782555+2172017@users.noreply.github.com>
2024-06-25 00:14:46 +0700

    Update index.blade.php

--- resources/views/user_prodi/index.blade.php
+++ resources/views/user_prodi/index.blade.php
@@ -6,9 +6,9 @@
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <diva class="col-sm-6">
+                    <div class="col-sm-6">
                         <h1 class="m-0">Program Studi</h1>
-                    </diva>
+                    </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
@@ -24,9 +24,9 @@
         <div class="content">
             <div class="container-fluid">
                 <div class="card p-4">
-
                     <div class="card-body">
-                        <table class="table table-striped" id="table-prodi">
+                        <h2>Daftar Beasiswa</h2>
+                        <table class="table table-striped table-bordered" id="table-prodi" style="width:100%">
                             <thead>
                             <tr>
                                 <th>ID</th>
@@ -56,14 +56,45 @@
 
 @section('ExtraCSS')
     <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <style>
+        .content-wrapper {
+            overflow-x: auto;
+        }
+        .card {
+            width: 100%;
+        }
+        .table thead th {
+            text-align: center;
+        }
+        .card-body h2 {
+            margin-bottom: 20px;
+            text-align: center;
+            font-weight: bold;
+            color: #007bff;
+        }
+    </style>
 @endsection
 
 @section('ExtraJS')
     <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
     <script>
         $(document).ready(function() {
-            $('#table-prodi').DataTable();
+            $('#table-prodi').DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#table-prodi_wrapper .col-md-6:eq(0)');
         });
     </script>
 @endsection
