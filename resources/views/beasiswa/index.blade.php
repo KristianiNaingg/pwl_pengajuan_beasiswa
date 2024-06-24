e1229c4131039fecbcc6b46107e2b63eb7385730
Theresia <90782555+2172017@users.noreply.github.com>
2024-06-25 00:13:13 +0700

    Update index.blade.php

--- resources/views/beasiswa/index.blade.php
+++ resources/views/beasiswa/index.blade.php
@@ -2,15 +2,13 @@
 
 @section('web-content')
     <div class="content-wrapper">
-
-
         <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <diva class="col-sm-6">
+                    <div class="col-sm-6">
                         <h1 class="m-0">Program Studi</h1>
-                    </diva>
+                    </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
@@ -26,7 +24,6 @@
         <div class="content">
             <div class="container-fluid">
                 <div class="card p-4">
-
                     <div class="card-body">
                         <table class="table table-striped" id="table-prodi">
                             <thead>
@@ -35,7 +32,7 @@
                                 <th>ID Jenis Beasiswa</th>
                                 <th>ID Periode</th>
                                 <th>Nama Beasiswa</th>
-                                <th>  Aksi </th>
+                                <th>Aksi</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -45,8 +42,7 @@
                                     <td>{{ $beasiswas->id_jenis_beasiswa}}</td>
                                     <td>{{ $beasiswas->id_periode}}</td>
                                     <td>{{ $beasiswas->nama_beasiswa}}</td>
-                                    <td><a href="{{ route('prodi_mahasiswa') }}"> Detail </a></td>
-
+                                    <td><a href="{{ route('prodi_mahasiswa') }}" class="btn btn-info btn-sm">Detail</a></td>
                                 </tr>
                             @endforeach
                             </tbody>
@@ -61,14 +57,25 @@
 
 @section('ExtraCSS')
     <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('ExtraJS')
     <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
     <script>
         $(document).ready(function() {
-            $('#table-prodi').DataTable();
+            $('#table-prodi').DataTable({
+                "responsive": true, "lengthChange": false, "autoWidth": false,
+            }).buttons().container().appendTo('#table-prodi_wrapper .col-md-6:eq(0)');
         });
     </script>
 @endsection
