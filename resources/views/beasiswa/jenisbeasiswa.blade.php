72cbc6838b822cf3251540abbb0282c15615cb08
Theresia <90782555+2172017@users.noreply.github.com>
2024-06-25 00:13:45 +0700

    Update jenisbeasiswa.blade.php

--- resources/views/beasiswa/jenisbeasiswa.blade.php
+++ resources/views/beasiswa/jenisbeasiswa.blade.php
@@ -2,15 +2,13 @@
 
 @section('web-content')
     <div class="content-wrapper">
-
-
         <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <diva class="col-sm-6">
+                    <div class="col-sm-6">
                         <h1 class="m-0">Program Studi</h1>
-                    </diva>
+                    </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
@@ -26,23 +24,21 @@
         <div class="content">
             <div class="container-fluid">
                 <div class="card p-4">
-
                     <div class="card-body">
-                        <table class="table table-striped" id="table-prodi">
+                        <table class="table table-striped table-bordered" id="table-prodi">
                             <thead>
                             <tr>
                                 <th>ID Jenis Beasiswa</th>
                                 <th>Nama Jenis Beasiswa</th>
-                                <th>  Aksi </th>
+                                <th>Aksi</th>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($jenis as $jensis)
                                 <tr>
-                                    <td>{{ $jensis->id_jenis_beasiswa}}</td>
-                                    <td>{{ $jensis->nama_jenis_beasiswa}}</td>
-                                    <td><a href="{{ route('prodi_beasiswa') }}"> Detail </a></td>
-
+                                    <td>{{ $jensis->id_jenis_beasiswa }}</td>
+                                    <td>{{ $jensis->nama_jenis_beasiswa }}</td>
+                                    <td><a href="{{ route('prodi_beasiswa') }}" class="btn btn-info btn-sm">Detail</a></td>
                                 </tr>
                             @endforeach
                             </tbody>
@@ -57,6 +53,7 @@
 
 @section('ExtraCSS')
     <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('ExtraJS')
@@ -64,7 +61,11 @@
     <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
     <script>
         $(document).ready(function() {
-            $('#table-prodi').DataTable();
+            $('#table-prodi').DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+            });
         });
     </script>
 @endsection
